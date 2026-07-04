307b8dd9b52e5e07d10cc856ad906c9a97bfb13a
Mulovhedzi Orifha Nthabiseng <orifha03mulovhedzi@gmail.com>
2026-07-04 15:57:29 +0200

    upload files

---
+++ demo.java
@@ -0,0 +1,8 @@
+
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.print("This is a demo Java program.");
+    }
+}
